97ed4f0684cdbc62241c6fde2bfc49d3413f0933
Zibri <Zibri@users.noreply.github.com>
2019-02-10 13:44:25 +0200

    Added mount point

--- get_device.c
+++ get_device.c
@@ -1,5 +1,5 @@
 /*
-Get physical device from file or directory name.
+Get physical device and mount point of a file or directory.
 By Zibri <zibri AT zibri DOT org>
 */
 
@@ -13,61 +13,62 @@ By Zibri <zibri AT zibri DOT org>
 #include <stdlib.h>
 #include <libgen.h>
 
-int
-get_device (char *name)
+int get_device(char *name)
 {
   struct stat fs;
 
-  if (stat (name, &fs) < 0)
-    {
-      fprintf (stderr, "%s: No such file or directory\n", name);
-      return -1;
-    }
+  if (stat(name, &fs) < 0) {
+    fprintf(stderr, "%s: No such file or directory\n", name);
+    return -1;
+  }
 
   FILE *f;
   char sline[256];
   char minmaj[128];
 
-  sprintf (minmaj, "%d:%d ", (int) fs.st_dev >> 8, (int) fs.st_dev & 0xff);
+  sprintf(minmaj, "%d:%d ", (int) fs.st_dev >> 8, (int) fs.st_dev & 0xff);
 
-  f = fopen ("/proc/self/mountinfo", "r");
+  f = fopen("/proc/self/mountinfo", "r");
 
-  if (f == NULL)
-    {
-      fprintf (stderr, "Failed to open /proc/self/mountinfo\n");
-      exit (-1);
+  if (f == NULL) {
+    fprintf(stderr, "Failed to open /proc/self/mountinfo\n");
+    exit(-1);
+  }
+
+  while (fgets(sline, 256, f)) {
+
+    char *token;
+    char *where;
+
+    token = strtok(sline, "-");
+    where = strstr(token, minmaj);
+    if (where) {
+      token = strtok(NULL, " -:");
+      token = strtok(NULL, " -:");
+      printf("%s", token);
+      token = strtok(sline, " ");  // ID
+      token = strtok(NULL, " ");   // Parent ID
+      token = strtok(NULL, " ");   // Major:Minor
+      token = strtok(NULL, " ");   // Parent mount point
+      printf("test:>%s<\n",token);
+      token = strtok(NULL, " ");   // Mount point
+      printf(" (%s)\n", token);
+      break;
     }
 
-  while (fgets (sline, 256, f))
-    {
-      char *token;
-      char *where;
-
-      token = strtok (sline, "-");
-      where = strstr (token, minmaj);
-      if (where)
-      	{
-      	  token = strtok (NULL, " -:");
-      	  token = strtok (NULL, " -:");
-      	  printf ("%s\n", token);
-          break;
-      	}
-    }
+  }
 
   return -1;
-  fclose (f);
+  fclose(f);
 }
 
-int
-main (int argc, char **argv)
+int main(int argc, char **argv)
 {
 
-  if (argc != 2)
-    {
-      fprintf (stderr, "Usage:\n%s FILE OR DIRECTORY...\n",
-	       basename (argv[0]));
-      return -1;
-    }
-  get_device (argv[1]);
+  if (argc != 2) {
+    fprintf(stderr, "Usage:\n%s FILE OR DIRECTORY...\n", basename(argv[0]));
+    return -1;
+  }
+  get_device(argv[1]);
   return 0;
 }
